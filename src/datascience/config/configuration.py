8ac7165296bbd971ca30667c8aaf839bdaa29f8e
Prabh-2004 <prabhjsm@gmail.com>
2026-04-02 22:32:30 +0530

    Added Data Transformation

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -2,6 +2,7 @@ from src.datascience.constants import *
 from src.datascience.utils.common import read_yaml, create_directories
 from src.datascience.entity.config_entity import DataIngestionConfig
 from src.datascience.entity.config_entity import DataValidationConfig
+from src.datascience.entity.config_entity import DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH, schema_filepath=SCHEMA_FILE_PATH):
@@ -37,4 +38,18 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.dataTransformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
+    
+
     
